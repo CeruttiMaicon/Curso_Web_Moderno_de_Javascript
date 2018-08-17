5191bf9ed226d34f4b2e7e376e3c1c5207ab87c1
regexp <=>
2018-08-17 13:11:15 -0300

    Secao 3 aula 14 - Declaração e tipos de variaveis no javascript

---
+++ JavaScript fundamentos - Seção 3/Aula 14/variaveisEConstantes.js
@@ -0,0 +1,25 @@
+// Criando uma variavel
+var a = 3
+
+// Criando uma variavel com o let
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+
+const c = 5
+/* Nao se pode atribuir um valor a uma constante, 
+ *   pelo motivo de ser uma constante
+ * 
+ *   c = 500   //nao funcionaria
+ */
+
+console.log(c)
